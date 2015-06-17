915ed37f26e726351cbf312c5eaab4fccb9130d6
Daniel Vetter <daniel.vetter@ffwll.ch>
2015-06-17 17:32:33 +0200

    [DRAFT] dma-buf: Add ioctls to allow userspace to flush
    FIXME: Update kerneldoc for begin/end to make it clear that those are
    for mmap too.
    
    Open: Do we need a special indication that the begin/end is from
    userspace mmap and not from kernel mmap?
    
    There's also the question already about kernel internal users - vmap
    and kmap interfaces are much different ... We might need to add a
    mapping enum to the begin/end dma-buf functions.
    
    Also totally untested.
    
    Missing patches:
    - Allow OPEN_RW in drm prime export, but only when dma-buf mmap is
      there.
    - Implement a suitable dma-buf mmap forwarder for i915.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>

---
+++ include/uapi/linux/dma-buf.h
@@ -0,0 +1,39 @@
+/*
+ * Framework for buffer objects that can be shared across devices/subsystems.
+ *
+ * Copyright(C) 2015 Intel Ltd
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _DMA_BUF_UAPI_H_
+#define _DMA_BUF_UAPI_H_
+
+struct dma_buf_sync {
+	u64 flags;
+	u64 start;
+	u64 length;
+};
+
+#define DMA_BUF_SYNC_READ	(1 << 0)
+#define DMA_BUF_SYNC_WRITE	(2 << 0)
+#define DMA_BUF_SYNC_RW		(3 << 0)
+#define DMA_BUF_SYNC_START	(0 << 2)
+#define DMA_BUF_SYNC_END	(1 << 2)
+#define DMA_BUF_SYNC_VALID_FLAGS_MASK \
+	(DMA_BUF_SYNC_RW | DMA_BUF_SYNC_END)
+
+#define DMA_BUF_BASE		'b'
+#define DMA_BUF_IOCTL_SYNC	_IOWR(DMA_BUF_BASE, 0, struct dma_buf_sync)
+
+#endif
