f4602e8be47a47d0a49dff17a54f79db13bbd9da
Rob Clark <robdclark@gmail.com>
2017-10-24 10:46:28 -0400

    fixup! drm/msm: Add per-instance submit queues

--- include/uapi/drm/msm_drm.h
+++ include/uapi/drm/msm_drm.h
@@ -281,10 +281,10 @@ struct drm_msm_submitqueue {
 #define DRM_MSM_WAIT_FENCE             0x07
 #define DRM_MSM_GEM_MADVISE            0x08
 /* placeholder:
-#define DRM_MSM_GEM_SVM_NEW              0x09
+#define DRM_MSM_GEM_SVM_NEW            0x09
  */
-#define DRM_MSM_SUBMITQUEUE_NEW          0x0A
-#define DRM_MSM_SUBMITQUEUE_CLOSE        0x0B
+#define DRM_MSM_SUBMITQUEUE_NEW        0x0A
+#define DRM_MSM_SUBMITQUEUE_CLOSE      0x0B
 
 #define DRM_IOCTL_MSM_GET_PARAM        DRM_IOWR(DRM_COMMAND_BASE + DRM_MSM_GET_PARAM, struct drm_msm_param)
 #define DRM_IOCTL_MSM_GEM_NEW          DRM_IOWR(DRM_COMMAND_BASE + DRM_MSM_GEM_NEW, struct drm_msm_gem_new)
