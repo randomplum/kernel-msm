af502184e426e7dad1f4ce364560ea7487d87e70
Chandan Uddaraju <chandanu@codeaurora.org>
2018-04-18 12:45:14 -0700

    FROMLIST: drm/msm/dsi: adjust dsi timing for dual dsi mode
    For dual dsi mode, the horizontal timing needs
    to be divided by half since both the dsi controllers
    will be driving this panel. Adjust the pixel clock and
    DSI timing accordingly.
    
    Changes in V2:
    --Removed Change-Id from the commit text tags.
    
    Changes in V3:
    --Instead of adjusting the DRM mode structure, divide
      the clocks and horizontal timings in DSI host just
      before configuring the values.
    
    Change-Id: I13d6dc90cabc85bbe53e760a1fb825974a337378
    Reviewed-by: Archit Taneja <architt@codeaurora.org>
    Signed-off-by: Chandan Uddaraju <chandanu@codeaurora.org>
    (from https://lists.freedesktop.org/archives/freedreno/2018-April/002323.html)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/msm/dsi/dsi.h
+++ drivers/gpu/drm/msm/dsi/dsi.h
@@ -162,7 +162,8 @@ void msm_dsi_host_cmd_xfer_commit(struct mipi_dsi_host *host,
 int msm_dsi_host_enable(struct mipi_dsi_host *host);
 int msm_dsi_host_disable(struct mipi_dsi_host *host);
 int msm_dsi_host_power_on(struct mipi_dsi_host *host,
-			struct msm_dsi_phy_shared_timings *phy_shared_timings);
+			struct msm_dsi_phy_shared_timings *phy_shared_timings,
+			bool is_dual_dsi);
 int msm_dsi_host_power_off(struct mipi_dsi_host *host);
 int msm_dsi_host_set_display_mode(struct mipi_dsi_host *host,
 					struct drm_display_mode *mode);
@@ -175,7 +176,8 @@ int msm_dsi_host_set_src_pll(struct mipi_dsi_host *host,
 			struct msm_dsi_pll *src_pll);
 void msm_dsi_host_reset_phy(struct mipi_dsi_host *host);
 void msm_dsi_host_get_phy_clk_req(struct mipi_dsi_host *host,
-	struct msm_dsi_phy_clk_request *clk_req);
+	struct msm_dsi_phy_clk_request *clk_req,
+	bool is_dual_dsi);
 void msm_dsi_host_destroy(struct mipi_dsi_host *host);
 int msm_dsi_host_modeset_init(struct mipi_dsi_host *host,
 					struct drm_device *dev);
