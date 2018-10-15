d4b8b9e97d00a6d385a7ad51de931136806ed2e4
Matthias Kaehlcke <mka@chromium.org>
2018-10-15 15:39:10 -0700

    FROMLIST: Bluetooth: Fix locking in bt_accept_enqueue() for BH context
    With commit e16337622016 ("Bluetooth: Handle bt_accept_enqueue() socket
    atomically") lock_sock[_nested]() is used to acquire the socket lock
    before manipulating the socket. lock_sock[_nested]() may block, which
    is problematic since bt_accept_enqueue() can be called in bottom half
    context (e.g. from rfcomm_connect_ind()).
    
    The socket API provides bh_lock_sock[_nested]() to acquire the socket
    lock in bottom half context. Check the context in bt_accept_enqueue()
    and use the appropriate locking mechanism for the context.
    
    Fixes: e16337622016 ("Bluetooth: Handle bt_accept_enqueue() socket atomically")
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    (am from https://lore.kernel.org/patchwork/patch/999941/)
    (also found at https://lkml.kernel.org/r/20181015223910.197729-1-mka@chromium.org)
    
    BUG=b:117401779
    TEST=No crash
    
    Change-Id: I1db784a2e088b7719845fab2358aa14d08cd0c73
    Signed-off-by: Douglas Anderson <dianders@chromium.org>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -188,10 +188,20 @@ void bt_accept_enqueue(struct sock *parent, struct sock *sk)
 	BT_DBG("parent %p, sk %p", parent, sk);
 
 	sock_hold(sk);
-	lock_sock_nested(sk, SINGLE_DEPTH_NESTING);
+
+	if (in_atomic())
+		bh_lock_sock_nested(sk);
+	else
+		lock_sock_nested(sk, SINGLE_DEPTH_NESTING);
+
 	list_add_tail(&bt_sk(sk)->accept_q, &bt_sk(parent)->accept_q);
 	bt_sk(sk)->parent = parent;
-	release_sock(sk);
+
+	if (in_atomic())
+		bh_unlock_sock(sk);
+	else
+		release_sock(sk);
+
 	parent->sk_ack_backlog++;
 }
 EXPORT_SYMBOL(bt_accept_enqueue);
